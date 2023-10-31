a84b11ce8a958bb9e6851989922441ce500f891b
Manas_D <dmanas22@hotmail.com>
2023-10-31 15:54:12 +0530

    Learned the use cases of the swtich-case statements.

---
+++ fullStack/javaScript-journey/04_control_flow/02_switch_case.js
@@ -0,0 +1,28 @@
+// This is the standard switch case statement.
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "jan"
+
+switch (month) {
+    case 'jan' : 
+    console.log('January');
+    break;
+    case 'feb': 
+    console.log("february");
+    break;
+    case 'March':
+    console.log("march");
+    break;
+    default:
+        console.log("default case match")
+
+}

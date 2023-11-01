f5bab578e14851485b8ada6633c48d15266e0f51
Manas_D <dmanas22@hotmail.com>
2023-11-02 04:31:12 +0530

    made some changes

--- two.js
+++ two.js
@@ -31,17 +31,3 @@
 // } while (score === true)
 
 
-
-// program to display numbers from 1 to 5
-// initialize the variable
-
-
-// program to display numbers from 1 to 5
-// initialize the variable
-let i = 1, n = 5;
-
-// while loop from i = 1 to 5
-while (i <= n) {
-    console.log(i);
-    i += 1;
-}

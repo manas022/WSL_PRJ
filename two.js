558c1e392a511b45d71adb6f9aa8cbc676be7ac0
Manas_D <dmanas22@hotmail.com>
2023-11-01 23:32:32 +0530

    made changes

--- two.js
+++ two.js
@@ -36,4 +36,12 @@
 // initialize the variable
 
 
+// program to display numbers from 1 to 5
+// initialize the variable
+let i = 1, n = 5;
 
+// while loop from i = 1 to 5
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}

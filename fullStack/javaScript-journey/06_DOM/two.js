4633ae141a2fd8dc101f4ea8d97069f2f88e23da
Manas_D <dmanas22@hotmail.com>
2023-11-13 17:45:36 +0530

    Added three files where I have been discussing about the DOM and their use methods

---
+++ fullStack/javaScript-journey/06_DOM/two.js
@@ -0,0 +1,26 @@
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 2 * min); 
+  }
+  
+  function randomChallenge() {
+    const categories = ['intro', 'callbacks', 'closures', 'asynch', 'oop', 'recursion'];
+    let randomIndex = Math.floor(Math.random() * categories.length);
+    let category = categories[randomIndex];
+    let num
+    if (category == 'intro') {
+      num = randomNumber(1, 19);
+    } else if (category === 'callbacks') {
+        num = randomNumber(1, 24);
+    } else if (category === 'closures') {
+      num = randomNumber(1, 19);
+    } else if (category === 'asynch') {
+      num = randomNumber(1, 10);
+    } else if (category === 'oop') {
+      num = randomNumber(1, 15);
+    } else if (category === 'recursion') {
+      num = randomNumber(1, 10);
+    }
+    console.log(category + ' ' + num)
+  }
+  
+  randomChallenge()
